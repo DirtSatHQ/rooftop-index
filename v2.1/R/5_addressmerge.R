9d8134b9b75d360fddc0e09915642f9033ae0f52
Nathalie Descusse-Brown <nathalie.descusse@gmail.com>
2023-08-15 09:28:39 -0400

    feat: script updated to reflect parameters used to generata data currently in Bubble

--- v2.1/R/5_addressmerge.R
+++ v2.1/R/5_addressmerge.R
@@ -16,11 +16,11 @@ results = read_sf(results_file)
 mycrstransformedaddresses = st_transform(address,crs(results))
 
 #nyc_buildings_withadress <- st_join(results, mycrstransformedaddresses, st_intersects) -- THIS APPROACH DIDN'T WORK AND CAUSED MISMATCH BETWEEN FOOTPRINT AND ADDRESS WITH ADJACENT BUILDINGS
-nyc_buildings_withadress_points <- st_join(mycrstransformedaddresses,results,st_is_within_distance,dist = 4)
+nyc_buildings_withadress_points <- st_join(mycrstransformedaddresses,results,st_is_within_distance,dist = 3)
 
-write_sf(nyc_buildings_withadress_points,'/Volumes/NDB_HDD/final/final_geospatial/final_NYC_results_withaddress_st_is_within_distance_4_alladdresspoints.geojson')
+write_sf(nyc_buildings_withadress_points,'/Volumes/NDB_HDD/final/final_geospatial/final_NYC_results_withaddress_st_is_within_distance_3_alladdresspoints.geojson')
 
-nyc_buildings_withadress_poly <- st_join(results,mycrstransformedaddresses,st_is_within_distance,dist = 4)
+nyc_buildings_withadress_poly <- st_join(results,mycrstransformedaddresses,st_is_within_distance,dist = 3)
 
-write_sf(nyc_buildings_withadress_poly,'/Volumes/NDB_HDD/final/final_geospatial/final_NYC_results_withaddress_st_is_within_distance_4_allpolys.geojson')
+write_sf(nyc_buildings_withadress_poly,'/Volumes/NDB_HDD/final/final_geospatial/final_NYC_results_withaddress_st_is_within_distance_3_allpolys.geojson')
 
